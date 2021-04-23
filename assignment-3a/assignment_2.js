cd5a926593bac4ec8e7dcd89f7b8c30b17254a0c
Gustav H <hultgrengustav@gmail.com>
2021-04-23 10:08:04 +0200

    part 2 of 3a done

--- assignment-3a/assignment_2.js
+++ assignment-3a/assignment_2.js
@@ -1,10 +1,29 @@
+let inputFields = $("input");
+var send;
+
 // När formuläret skickas (dvs. när användaren klickar på submit-knappen).
-$("#newsletter").on("submit", function(e) {
-    // Hindra formuläret från att skicka iväg användaren.
-    e.preventDefault();
+$("#newsletter").on("submit", function (e) {
+  // Hindra formuläret från att skicka iväg användaren.
+  e.preventDefault();
 
-    // Här placerar ni er validerings kod.
+  send = true;
 
-    // Om allting var korrekt - skicka vidare användaren.
+  // Här placerar ni er validerings kod.
+  for (var i = 0; i < inputFields.length; i++) {
+    if (!inputFields[i].value) {
+      inputFields[i].style.backgroundColor = "red";
+      send = false;
+    } else {
+      inputFields[i].style.backgroundColor = "white";
+    }
+  }
+
+  // Om allting var korrekt - skicka vidare användaren.
+  if (send) {
     $("#newsletter")[0].submit();
+    for (var i = 0; i < inputFields.length; i++) {
+      inputFields[i].value = "";
+    }
+    alert("Sent!");
+  }
 });
