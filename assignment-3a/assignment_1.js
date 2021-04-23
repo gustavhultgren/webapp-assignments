72149bc4911d3ff36b0e136e7645ea0906b58cb8
Gustav H <hultgrengustav@gmail.com>
2021-04-23 09:35:24 +0200

    changed to multiplication

--- assignment-3a/assignment_1.js
+++ assignment-3a/assignment_1.js
@@ -3,7 +3,7 @@ $("#nr-1, #nr-2").keyup(function () {
   if ($("#nr-1").val() && $("#nr-2").val()) {
     var number1 = parseInt($("#nr-1").val());
     var number2 = parseInt($("#nr-2").val());
-    sum = number1 + number2;
+    sum = number1 * number2;
     $("#result").val(sum);
   }
 });
